2e6cab571310baa90689da1de08782f5c98bc19d
awadesu <133776560+awadesu@users.noreply.github.com>
2023-05-26 10:18:39 +0200

    create grid and working button

---
+++ script.js
@@ -0,0 +1,47 @@
+
+const body = document.querySelector("body");
+const button = document.createElement("button");
+let big = document.createElement("div");
+let size = 16;
+
+
+function grid(n){
+    for (let i = 0; i < n; i++) {
+        const small = document.createElement('div');
+        small.classList = "column";
+        for (let j = 0; j < n; j++) {
+            const tiny = document.createElement("div");
+            tiny.classList = "square";
+            small.appendChild(tiny);
+        }
+        big.appendChild(small);
+    }
+}
+
+
+
+function setGrid(n) {
+    big.innerHTML =""; 
+    console.log(big.children);
+    if (n <= 100 && n >= 0) {
+        grid(n);
+    } else {
+        setGrid(size);
+        setGrid(prompt("Please choose a grid size between 0 and 100"));
+    }
+    big.id = "big";
+    body.appendChild(big);
+}
+
+button.innerText = "Select a grid size";
+button.addEventListener('click', () => {
+    console.log("button clicked!");
+    console.log(big.children);
+    setGrid(prompt("Please choose a grid size between 0 and 100"));
+});
+
+body.appendChild(button);
+
+setGrid(size);
+
+
